610443a75c3de8b1c101ede8e112d8f2e18d88d9
Zhenglin Li <1125806272@qq.com>
2022-09-03 11:05:31 +0800

    fix_bug

--- projects/blog/comments/index.js
+++ projects/blog/comments/index.js
@@ -7,21 +7,20 @@ app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
-app.get('posts/:id/comments', (req, res) => {
-    res.send("commentsByPostId[req.params.id] || []");
+app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
-    const comments = commentId[req.params.id] || [];
+    const comments = commentsByPostId[req.params.id] || [];
 
     comments.push({ id: commentId, content });
 
     commentsByPostId[req.params.id] = comments;
-    res.statusCode(201).send(comments)
+    res.status(201).send(comments)
 });
 
 app.listen(4005, () => {
