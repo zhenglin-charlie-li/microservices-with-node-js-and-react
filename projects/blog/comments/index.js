83c06ecb4232b971ada0ad3746be7d06bbf2abc8
Zhenglin Li <1125806272@qq.com>
2022-09-06 14:01:43 +0800

    02-036 Handling Moderation

--- projects/blog/comments/index.js
+++ projects/blog/comments/index.js
@@ -22,7 +22,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
 
     commentsByPostId[req.params.id] = comments;
 
@@ -31,7 +31,8 @@ app.post('/posts/:id/comments', async (req, res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     })
 
--- projects/blog/event-bus/index.js
+++ projects/blog/event-bus/index.js
@@ -8,14 +8,21 @@ app.use(bodyParser.json())
 app.post('/events', (req, res) => {
     const event = req.body
 
+    // posts
     axios.post('http://localhost:4000/events', event).catch((err) => {
         console.log(err.message, '4000');
     });
+    // comments
     axios.post('http://localhost:4001/events', event).catch((err) => {
         console.log(err.message, '4001');
     });
+    // query
     axios.post('http://localhost:4002/events', event).catch((err) => {
         console.log(err.message, '4002');
+    });
+    // moderation
+    axios.post('http://localhost:4003/events', event).catch((err) => {
+        console.log(err.message, '4003');
     });
     res.send({ status: 'OK' });
 });
