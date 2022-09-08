ceba5d74331db4aa20336b060dafdd26e315fc4e
Zhenglin Li <1125806272@qq.com>
2022-09-08 16:00:26 +0800

    02-043 Event Syncing in Action

--- projects/blog/event-bus/index.js
+++ projects/blog/event-bus/index.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+const events = [];
 
 app.post('/events', (req, res) => {
     const event = req.body
+
+    events.push(event);
 
     // posts
     axios.post('http://localhost:4000/events', event).catch((err) => {
@@ -26,6 +30,10 @@ app.post('/events', (req, res) => {
     });
     res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
     console.log('listening on 4005');
