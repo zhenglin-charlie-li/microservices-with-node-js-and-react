54e70b4d33bcb0f961d1dbb446a5b0670fad09fe
zhenglinli <1125806272@qq.com>
2022-12-01 22:33:35 +0800

    04-026

--- projects/blog/event-bus/index.js
+++ projects/blog/event-bus/index.js
@@ -16,18 +16,18 @@ app.post('/events', (req, res) => {
     axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {
         console.log(err.message, '4000');
     });
-    // // comments
-    // axios.post('http://localhost:4001/events', event).catch((err) => {
-    //     console.log(err.message, '4001');
-    // });
-    // // query
-    // axios.post('http://localhost:4002/events', event).catch((err) => {
-    //     console.log(err.message, '4002');
-    // });
-    // // moderation
-    // axios.post('http://localhost:4003/events', event).catch((err) => {
-    //     console.log(err.message, '4003');
-    // });
+    // comments
+    axios.post('http://comments-srv:4001/events', event).catch((err) => {
+        console.log(err.message, '4001');
+    });
+    // query
+    axios.post('http://query-srv:4002/events', event).catch((err) => {
+        console.log(err.message, '4002');
+    });
+    // moderation
+    axios.post('http://moderation-srv:4003/events', event).catch((err) => {
+        console.log(err.message, '4003');
+    });
     res.send({ status: 'OK' });
 });
 
